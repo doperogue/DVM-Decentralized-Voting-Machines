0659a974f348ecee9a5b6ab1ee9cc75b834c33f6
Tenzin Jamtsho <35904512+sandwizard@users.noreply.github.com>
2020-08-26 22:05:23 +0600

    changes

--- src/block.py
+++ src/block.py
@@ -3,12 +3,24 @@ from hashlib import sha256
 
 class Block:
     # block class
-    def __init__(self, id, previous_hash,vote,nounce=0):
+    def __init__(self,index id, previous_hash,vote,nonce=0):
         #initialise block note create genisis node creation
+        # thought of using index for difficulty but ?
+        #edit dont use index for difficulty and it isnt a requirement
+        #for our systema hacker could potentaly modify every index making difficulty low so stick t position in array
         self.id = id 
-        self.vote= vote
+        self.vote = vote
         self.previous_hash = previous_hash
-        self.nounce= nounce
+        self.nonce= nonce
+        
+
+    def print_block(self,proof):
+        print("hash: " + str(proof))
+        print("id:"+ str(self.id))
+        print("prev_hash:"+ str(self.previous_hash))
+        print("vote"+ str(self.vote)) 
+        print("nonce: "+str(self.nonce))
+        
 
 
 
@@ -16,22 +28,23 @@ class Block:
     def compute_hash(self):
         # re work instead of hash check for id in stopping revoting should still work and less complexity
         block_string = json.dumps(self.__dict__, sort_keys=True)
+        #for debuging remove later
         print(sha256(block_string.encode()).hexdigest())
         return sha256(block_string.encode()).hexdigest()
 
 #test if hash remains consistent works
 #r1=Block(342434,34234,'trevt')
 #r1.compute_hash()
-
-
-# instead of arry might want to store chain in form of dictionary with keys as id for quick look up and for duplicates in revoting
+          
+# instead of arry might want to store chain in form of dictionary with keys 
+#as id for quick look up and for duplicates in revoting
 
 class Blockchain():
     """blockchain class"""
     def __init__(self):
         self.chain = []
         self.create_genesis_block()
-    def create_genesis_block(self,id):
+    def create_genesis_block(self):
         genesis_block = Block(0, 0,0)
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
@@ -46,11 +59,13 @@ class Blockchain():
     def proof_of_work(self, block):
         # the one who creates the block will have to find the right nounce value for each difficulty 
         #and complexity increases as dificulty increases
-        block.nonce = 0
+        block.nonce=0
         computed_hash = block.compute_hash()
         while not computed_hash.startswith('0' * Blockchain.difficulty):
             block.nonce += 1
             computed_hash = block.compute_hash()
+            
+        
         return computed_hash
     def add_block(self, block, proof):
         #block is added in each node in p2p with proof provied by initial block creatoror miner and checked if it is valid
@@ -63,10 +78,48 @@ class Blockchain():
         block.hash = proof
         self.chain.append(block)
         return True
- 
+    #in p2p network should i instead of verifying this vay verify by comparison to other nodes?
     def is_valid_proof(self, block, block_hash):
         return (block_hash.startswith('0' * Blockchain.difficulty) and
                 block_hash == block.compute_hash())
 
+    #test func to print blocks
+    def print_blocks(self):
+        for blocks in self.chain:
+            print("hash: "+str(blocks.hash))
+            print("id:"+ str(blocks.id))
+            print("prev_hash:"+ str(blocks.previous_hash))
+            print("vote"+ str(blocks.vote))
+            print("nonce"+ str(blocks.nonce))
 
 
+#test 1 
+#for if genisis block is created works
+print("test 1 ")
+chain = Blockchain()
+chain.print_blocks()
+
+#test 2 
+#for if proof of work gets calculated
+print("test 2 ")
+block1 = Block(353424, chain.last_block.hash,"Me")
+proof_block1= chain.proof_of_work(block1)
+print( "proof: "+ str(proof_block1))
+
+#test 3
+# for if nounce is updated
+print("test 3 ")
+block1.print_block( proof_block1)
+
+#test 4 
+# if block is valid add to cahin
+#when sending block to other nodes always send precalculated hash so we can check if node has been tampered with
+print ("test 4")
+if(chain.is_valid_proof(block1, proof_block1)):
+    print("valid")
+
+#test 5
+#add block to chain and verify
+print ("test 5")
+chain.add_block(block1,proof_block1)
+chain.print_blocks()
--- src/program.py
+++ src/program.py
@@ -1,11 +1,30 @@
-import block
+import block as bl
 
 
-chain = block.Blockchain()
 # create genesis block 
+chain = bl.Blockchain()
 
 
 # create block based on input from user after verification
+class vote:
+    def __init__(self, id,vote):
+        #initialise block note create genisis node creation
+        self.id = id 
+        self.vote= vote
+        self.vote_block = bl.Block(id,chain.last_block.hash,vote)
+    #def checkvote(self)
+        #function to check if vote exist in chain
+        #use dictionry 
+
+    def submit_vote(self)
+        proof = bl.proof_of_work(chain,vote_block)
+        
+
+
+    
+ 
+        
+        
 
 
 
