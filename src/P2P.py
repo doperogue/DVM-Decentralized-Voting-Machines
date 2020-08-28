a8a9bd0a86734febcea5b5e690bcd97f10f38f57
Tenzin Jamtsho <35904512+sandwizard@users.noreply.github.com>
2020-08-29 02:12:57 +0600

    p2p implemented but need to modify for node vote interface and change port scanner to chack ip that have visited domain instead ot local ip range

--- src/P2P.py
+++ src/P2P.py
@@ -6,7 +6,7 @@ import traceback
 import json
 from asyncio.exceptions import TimeoutError as ConnectionTimeoutError
 # class for host
-
+HELLO_MY_NAME_IS ='server'
     #determi the host name and ip
     # add a way to find ip and by trying 
     #getes ip
@@ -21,11 +21,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
 print(MY_IP)
 # for referencing instance of task so when reference stops instance stops python garbage collec
-CONNECTIN = set()
+CONNECTINS= set()
 # creata a super class fo all peers
 class ConnectionHandeler:
     websocket = None
     hostname = None
+    uri = None
     state = 'Disconnect'
 
     async def send(self,message):
@@ -42,39 +43,86 @@ class ConnectionHandeler:
         except:
             traceback.print_exc() 
 
+            #client sending
+    async def login(self):
+        try:
+            self.websocket = await asyncio.wait_for(websockets.connect(self.uri),timeout=3)
+        except ConnectionTimeoutError:
+            print(f'ConnectionTimeout to:{self.uri}')
+            return
+        except ConnectionRefusedError:
+            print(f'Connection refused to{self.uri}')
+        except:
+            return
+        await self.send({'hostname':HELLO_MY_NAME_IS})
+
+        challenge = await self.recv()
+
+
+
+        if 'challenge' not in challenge or 'hostname' not in challenge:
+            
+            return
+        if len(challenge['challenge'])>1024 or len(challenge['hostname']) >1024:
+
+            return
+
+        #TODO: password hashing Goes here
+        self.hostname = challenge['hostname']
+        password ={'password':'password'}
+
+        await self.send(password)
+
+
+        confirmation = await self.recv()
+        confirmed = confirmation.get('connection')
+        if confirmed =='authorised':
+
+            self.state = 'Connected'
+            print(f'connected to :{self.hostname}')
+            return
+
+
+
+    #client/peer connection
     
 
     async def welcome(self) -> bool:
+        #serve/peer receiving
         #initial message is a declaration od a peer and seed important info to set up conn
         greeting = await self.recv()
 
         if 'hostname' not in greeting:
             return False
         #to prevent hackers dont accept insanley large values
-        if len(greeting['hostname']) > 1024;
+        if len(greeting['hostname']) > 1024:
             return False
 
 
         self.hostname = greeting['hostname']
 # challenge peer to confirm legitamacy
-        challenge = {'challenge':'548thfweghwhruefewrewr'}
+        challenge = {'challenge':'548thfweghwhruefewrewr','hostname':HELLO_MY_NAME_IS}
 
         await self.send(challenge)
 
         password = await self.recv()
-
+        print(password)
 #todo add actual password
         if 'password' not in password:
             return False
         if len(password['password']) > 1024:
             return False
         if password['password'] == 'password':
+            await self.send({'connection':'authorised'})
+
             self.state ='Connected'
+            print(f'new connection from :{self.hostname}')
             #creates listener which is all ways present and ehn con is closed if it is deinstanced task will dissaper
             asyncio.get_event_loop().create_task(self.listener())
             return True
-
-        return False
+        else:
+            await self.send({'connection':'unauthorised'})
+            return False
 
 
 
@@ -84,19 +132,28 @@ class ConnectionHandeler:
             async for message in self.websocket:
                 print(message)
         except websockets.exceptions.ConnectionClosed:
+
             print(f"Connection closed drom {self.hostname}")
+        except:
+            traceback.print_exc()
+        finally:
+            await unregister(self)
  
-    async def close(self):
+    async def close(self): 
         try:
-            self.websocket.close()
+            self.state='disconnected'
+            await self.websocket.close()
         except:
             traceback.print_exc()
 
-class ServerHandeler(ConnectionHandelern):
+class ServerHandeler(ConnectionHandeler):
     def __init__(self, websocket):
         self.websocket = websocket
-    
+class ClientHandeler(ConnectionHandeler):
+    def __init__(self,uri):
+        self.uri = uri
 
+#TODO chnge to initial seed  peers
     
 async def port_scan():
     #scans the local ips find any possible ip which connects to our port
@@ -117,32 +174,28 @@ async def port_scan():
         target_ip =f"{ip_range}.{i}"
         print(target_ip)
         uri =f"ws://{target_ip}:1111"
-        try:
-            connection = await asyncio.wait_for(websockets.connect(uri),timeout=5) 
-            await connection.send('hello')
-            async for message in connection:
-                print(message)
-        except ConnectionRefusedError:
-            print("server connection refused")
-            pass
-        except ConnectionError:
-            pass
-        except ConnectionTimeoutError:
-            print(f"timeout:{target_ip}")
-            pass
-        except TimeoutError:
-            pass
-        except websockets.exceptions.ConnectionClosed:
-            pass
-        except:
-            traceback.print_exc()
+        
+        connection = ClientHandeler(uri)
+        await connection.login()
+        if connection.state == 'Connected':
+            CONNECTINS.add(connection)
+            asyncio.get_event_loop().create_task(connection.listener())
 
+        await asyncio.sleep(0)
 
 
 async def register_client(websocket,_):
-    async for message in websocket:
-        print(message)
-        await websocket.send("hello yourself")
+    connection = ServerHandeler(websocket)
+    if await connection.welcome():
+        # the connec tion will persit in memory 
+        CONNECTINS.add(connection)
+async def unregister(connection):
+    await connection.close()
+    try:
+        CONNECTINS.remove(connection)
+    except:
+        traceback.print_exc()
+
 
 
 if __name__ == "__main__":
--- src/testing_client.py
+++ src/testing_client.py
@@ -6,5 +6,6 @@ async def client_things():
 	await P2P.port_scan()
 
 if __name__ == '__main__':
+	P2P.HELLO_MY_NAME_IS = 'client'
 	asyncio.get_event_loop().run_until_complete(client_things())
 	asyncio.get_event_loop().run_forever()
